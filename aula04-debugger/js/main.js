664eb70a91ae3ebf144ff95e8cded7620132b99f
rafduarte <69902106+rafduarte@users.noreply.github.com>
2020-09-11 11:02:00 -0300

    Aula04 de LPWB - 11/09/2020

---
+++ aula04-debugger/js/main.js
@@ -0,0 +1,37 @@
+let entradas = document.querySelectorAll('input');
+let btnSomar = document.querySelector('#btnSomar');
+let resposta = document.querySelector('.resposta');
+let num1 = entradas[0];
+let num2 = entradas[1];
+function pegarNum1() {
+    return num1.value;
+}
+ function pegarNum2() {
+    return num2.value;
+}
+function verificarEntradasVazias() {
+    if(pegarNum1() === '' || pegarNum2() === ''){return true;}
+    else{return false;}
+}
+function somar(){
+    if(verificarEntradasVazias()){atualizaResposta();}
+    else{atualizaResposta();}
+}
+btnSomar.addEventListener('click',somar);
+function atualizaResposta() {
+    if(verificarEntradasVazias()){
+        resposta.classList.remove('certa');
+        resposta.classList.add('errada');
+        debugger;
+        resposta.textContent = "Erro: Preencha os dois números!"
+    }else{
+        resposta.classList.remove('errada');
+        resposta.classList.add('certa');
+        let numero1 = pegarNum1();
+        numero1 = parseFloat(numero1);
+        let numero2 = pegarNum2();
+        numero2 = parseFloat(numero2);
+        let soma = numero1 + numero2;
+        resposta.textContent = `${numero1} + ${numero2} = ${soma}`;
+    }
+}
